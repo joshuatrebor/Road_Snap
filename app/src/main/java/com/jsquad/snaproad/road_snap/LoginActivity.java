401251f36aa968a6b4ae72860ef17ad7ae232920
JoeKaz <joshuatreborcastaneda@gmail.com>
2017-07-20 01:51:49 +0800

    Initial commit

---
+++ app/src/main/java/com/jsquad/snaproad/road_snap/LoginActivity.java
@@ -0,0 +1,62 @@
+package com.jsquad.snaproad.road_snap;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private Button btnLogin;
+    private TextView tvJoin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtEmail = (EditText)findViewById(R.id.txtEmail);
+        txtPassword = (EditText)findViewById(R.id.txtPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        tvJoin = (TextView)findViewById(R.id.tvJoin);
+
+        init();
+    }
+
+    private void init(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = authenticateUser(txtEmail.getText().toString(), txtPassword.getText().toString());
+                if(userId != null){
+                    Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
+                    intent.putExtra("userId", userId);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private String authenticateUser(String email, String password){
+        //TODO User Authertication
+        return "Abc";
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
