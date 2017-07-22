88dd59f02bd9a3bdbb66191aa8dda7372df7455b
JoeKaz <joshuatreborcastaneda@gmail.com>
2017-07-22 17:52:20 +0800

    lalala

--- app/src/main/java/com/jsquad/snaproad/road_snap/GPSTracker.java
+++ app/src/main/java/com/jsquad/snaproad/road_snap/GPSTracker.java
@@ -21,56 +21,74 @@ import com.google.android.gms.maps.model.LatLng;
 public class GPSTracker implements LocationListener {
 
     private Context context;
-    private RoadMap roadMap;
-
     private LocationManager locMan;
     private String provider;
-
     private Location location;
 
-    GPSTracker(Context context, RoadMap roadMap) {
+    GPSTracker(Context context) {
         this.context = context;
-        this.roadMap = roadMap;
-
         locMan = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         provider = locMan.getBestProvider(new Criteria(), true);
     }
 
-    public void start(){
+    public void start(long time, float distance, LocationListener locationListener){
         if(checkPermission()){
-            locMan.requestLocationUpdates(provider, 5000, 0, this);
+            if(locationListener != null){
+                locMan.requestLocationUpdates(provider, time, distance, locationListener);
+                Log.d("JOCAS", "GEOLOCATION STARTED USING GIVEN LISTENER");
+            }
+            else if(locationListener == null){
+                locMan.requestLocationUpdates(provider, time, distance, this);
+                Log.d("JOCAS", "GEOLOCATION STARTED USING GPSTRACKER LISTENER");
+            }
         }
+    }
+
+    public Location getLastKnownLocation(){
+
+        if(checkPermission()){
+            location = locMan.getLastKnownLocation(provider);
+            if(location != null) Log.d("JOCAS", "LAST KNOWN LOCATION RETURNED");
+            else Log.d("JOCAS", "LAST KNOWN LOCATION NOT AVAILABLE");
+
+            return location;
+        }
+        Log.d("JOCAS", "LAST KNOWN LOCATION NOT AVAILABLE");
+        return null;
+    }
+
+    public Location getCurrentLocation(){
+        Log.d("JOCAS", location == null ? "LOCATION NOT YET AVAILABLE":"LOCATION RETURNED");
+        return location;
     }
 
     public boolean checkPermission(){
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d("jocas", "Permission Denied");
+            Log.d("JOCAS", "LOCATION PERMISSION DENIED");
             return false;
         }
         return true;
     }
 
+    /**
+     * LOCATION lISTENER METHODS
+     *
+     */
+
     @Override
     public void onLocationChanged(Location location) {
         this.location = location;
+        Log.d("JOCAS", "LOCATION: " + location.getLatitude() + " " + location.getLongitude());
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     @Override
-    public void onProviderEnabled(String provider) {
-
-    }
+    public void onProviderEnabled(String provider) {}
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String provider) {}
 
-    }
 
-    public Location getLocation(){
-        return location;
-    }
 }
